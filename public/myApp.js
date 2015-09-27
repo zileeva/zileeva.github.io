976ee524650f4dda460d9d51b2ae4b1f26bc9817
Yulia  Zileeva <zileeva.y@husky.neu.edu>
2015-09-26 23:45:06 -0400

    created a route for instagram-hashtag

---
+++ public/myApp.js
@@ -0,0 +1,26 @@
+var myApp = angular.module('myApp', [
+	'ui.router',
+	'ui.bootstrap'
+
+	]);
+
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+		.state('instagram-hashtag', {
+			url: "/instagram-hashtag", templateUrl: 'instagram-hashtag/main.html', controller: 'InstagramHashtag'
+		});
+
+})
+.run(function($rootScope, $location, $window, $state) {
+	
+	
+});
+
+myApp.controller('InstagramHashtag', ['$scope', function($scope) {
+	console.log("InstagramHashtag controller is activated")
+}])
+
+
+
