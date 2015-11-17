2e0fe5ed8889eb3897040be6fdeaac35fa810ca3
Yulia  Zileeva <zileeva.y@husky.neu.edu>
2015-11-16 21:16:02 -0500

    Added DDA initial html mockups

---
+++ public/dda/js/modals.js
@@ -0,0 +1,151 @@
+$("#userForm").submit(function (event) {
+	console.log("here")
+	event.preventDefault();
+	$("#user").modal('hide');
+
+	$("#msgSent").show();
+
+	//setTimeout(funtion() { $("#msgSent").hide(); }, 2000);
+	setTimeout(function(){
+  $("#msgSent").hide();
+}, 3000);
+
+})
+$("#eventForm").submit(function (event) {
+	event.preventDefault()
+
+	var eventName = $("#eventForm #eventName").val();
+	var dateTime = $("#eventForm #dateTime").val();
+	var location = $("#eventForm #location").val();
+	var description = $("#eventForm #description").val();
+
+	var postTemplate = "<div class=\"col-lg-12 col-md-12\">" + 
+                        "<div class=\"panel panel-event\">" +
+                            "<div class=\"panel-heading\">" +
+                                "<div class=\"row\">" +
+                                    "<div class=\"col-xs-3\">" +
+                                        "<i class=\"fa fa-calendar fa-5x\"></i>" +
+                                    "</div>" +
+                                    "<div class=\"col-xs-9 text-right\">   " +
+                                        "<div class=\"huge\">" + eventName + "</div>" + 
+                                        "<div class=\"event-info\"> <u>When</u>: " + dateTime + "</div>" +     
+                                        "<div class=\"event-info\"> <u>Where</u>: " + location + "</div>" + 
+                                        "<div><i>" + description + "</i></div>" + 
+                                    "</div>" +
+
+
+                                "</div>" +
+
+                                "<div class=\"row\">" + 
+                                        "<div class=\"col-xs-6\">" + 
+                                            "<div class=\"pull-left poster\">" + 
+                                              "  <u>Posted by you</u>" + 
+                                           " </div>" + 
+                                        "</div>" + 
+                               " </div>" + 
+                                    
+                          "</div><a href=\"#\"><div class=\"panel-footer\"><span class=\"pull-left\">Add Comment</span><span class=\"pull-right\"><i class=\"fa fa-plus\"></i></span><div class=\"clearfix\"></div></div></a></div></div>";
+
+
+	$("#createEvent").modal('hide');
+	$("#feed").prepend(postTemplate);
+
+	$("#eventForm #eventName").val("");
+	$("#eventForm #dateTime").val("");
+	$("#eventForm #location").val("");
+	$("#eventForm #description").val("");
+
+})
+
+$("#postForm").submit(function (event) {
+	event.preventDefault()
+
+	var postName = $("#postForm #postName").val();
+	var postContent = $("#postForm #postContent").val();
+
+
+	var postTemplate = "<div class=\"col-lg-12 col-md-12\">" + 
+                        "<div class=\"panel panel-post\">" +
+                            "<div class=\"panel-heading\">" +
+                                "<div class=\"row\">" +
+                                    "<div class=\"col-xs-3\">" +
+                                        "<i class=\"fa fa-pencil-square-o fa-5x\"></i>" +
+                                    "</div>" +
+                                    "<div class=\"col-xs-9 text-right\">   " +
+                                        "<div class=\"huge\">" + postName + "</div>" +
+                                        
+                                        "<div>" +
+                                            "<p>" + postContent + 
+                                            "</p>" +
+                                        "</div>" +
+
+                                    "</div>" +
+
+
+                                "</div>" +
+
+                                "<div class=\"row\">" + 
+                                        "<div class=\"col-xs-6\">" + 
+                                            "<div class=\"pull-left poster\">" + 
+                                              "  <u>Posted by you</u>" + 
+                                           " </div>" + 
+                                        "</div>" + 
+                               " </div>" + 
+                                    
+                          "</div><a href=\"#\"><div class=\"panel-footer\"><span class=\"pull-left\">Add Comment</span><span class=\"pull-right\"><i class=\"fa fa-plus\"></i></span><div class=\"clearfix\"></div></div></a></div></div>";
+
+	$("#writePost").modal('hide')
+	$("#feed").prepend(postTemplate)
+
+	$("#postForm #postName").val("");
+	$("#postForm #postContent").val("");
+
+
+})
+
+
+$("#askForm").submit(function (event) {
+	event.preventDefault()
+
+	var askName = $("#askForm #askName").val();
+	var askContent = $("#askForm #askContent").val();
+
+
+	var postTemplate = "<div class=\"col-lg-12 col-md-12\">" + 
+                        "<div class=\"panel panel-red\">" +
+                            "<div class=\"panel-heading\">" +
+                                "<div class=\"row\">" +
+                                    "<div class=\"col-xs-3\">" +
+                                        "<i class=\"fa fa-question fa-5x\"></i>" +
+                                    "</div>" +
+                                    "<div class=\"col-xs-9 text-right\">   " +
+                                        "<div class=\"huge\">" + askName + "</div>" +
+                                        
+                                        "<div>" +
+                                            "<p>" + askContent + 
+                                            "</p>" +
+                                        "</div>" +
+
+                                    "</div>" +
+
+
+                                "</div>" +
+
+                                "<div class=\"row\">" + 
+                                        "<div class=\"col-xs-6\">" + 
+                                            "<div class=\"pull-left poster\">" + 
+                                              "  <u>Posted by you</u>" + 
+                                           " </div>" + 
+                                        "</div>" + 
+                               " </div>" + 
+                                    
+                          "</div><a href=\"#\"><div class=\"panel-footer\"><span class=\"pull-left\">Add Comment</span><span class=\"pull-right\"><i class=\"fa fa-plus\"></i></span><div class=\"clearfix\"></div></div></a></div></div>";
+
+	$("#askAdvice").modal('hide');
+	$("#feed").prepend(postTemplate);
+
+	$("#askForm #askName").val("");
+	$("#askForm #askContent").val("");
+
+
+})
--- public/myApp.js
+++ public/myApp.js
@@ -1,56 +1,29 @@
-// RUNS ANGULAR 
 
-var myApp = angular.module('myApp', [    // myApp - you can do whatever you want but 
-										//var anything = angular.module('HERE HAS TO BE SAME AS IN INDEX.HTML'
-		
-
-    // ANGULAR LIBRARIES										
-	'ui.router',  // This helps you to control different states and routers below  - router is /something 
-	'ui.bootstrap'  // THIS IS DEPENDENCY INJECTION 
+var myApp = angular.module('myApp', [   
+	'ui.router',  
+	'ui.bootstrap'  
 
 	]);
 
-
-// setting up routers USING Angular library ui.router 
-// $stateProvider & urlRouterProvider- comes from ui.router - need to inject here so you can use it in order to set up routers 
-
-// BLOCKS : myApp.config(...)  & .run(...)
-
-myApp.config(function($stateProvider, $urlRouterProvider) {   // define routers
+myApp.config(function($stateProvider, $urlRouterProvider) {  
 
 	$urlRouterProvider.otherwise("/me");
 
 	$stateProvider
 
-
-// url = real url  templateUrl = path to html file
-
-		.state('home', { // home/main.ctrl.js - controller here 
-			url: "/me", templateUrl: 'public/home/home.html', controller: 'Home' //activate controler called home (main.ctrl.js) 
-																				//replace ui-view in INDEX.HTML it will put home.html without reloading 
+		.state('home', { 
+			url: "/me", templateUrl: 'public/home/home.html', controller: 'Home' 
 		})
-
-
 		.state('instagram-hashtag', {
 			url: "/instagram-hashtag", templateUrl: 'public/instagram-hashtag/main.html', controller: 'InstagramHashtag'
-		});
+		})
+		.state('dda', {
+			url: "/dda", templateUrl: "public/dda/index.html"
+		})
 
 })
 
-// don't have to repeat myApp - can do just .run - implied 
 .run(function($rootScope, $location, $window, $state) {  
-
-	// when your app starts to run this is where we check 
-	// if the person is authenticated - if not then redirect 
-
-	// this happes right after someone goes on your website 
-	
-
 	
 });
-
-
-// controllers in another file - next 
-
-
 
