45e8e1a657eb002bd1555b376623bc97f200df1e
Yulia  Zileeva <yuliazileeva@yulias-air.homenet.telecomitalia.it>
2017-06-05 09:39:24 -0400

    Ponte Rotto in Rome timeline

--- app/Media.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-var Media = React.createClass({
-  render: function() {
-    return <img src={this.props.src} width={this.props.width} className='abs' />
-  }
-});
-
-export default Media;
